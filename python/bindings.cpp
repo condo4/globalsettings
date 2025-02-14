78ced30d39dad8090a19038c15653873d9d48579
Fabien Proriol <fabien.proriol@viavisolutions.com>
2025-02-14 11:19:46 +0100

    Manage set in the python binding

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -9,14 +9,27 @@ class PyGlobalSettings {
     GlobalSettings m_settings;
 
 public:
+    PyGlobalSettings();
     std::string __repr__();
     static void add_python_binding(pybind11::module &m);
     SettingValue get(std::string key, std::string category, SettingValue def);
+    SettingValue set(std::string key, SettingValue value, std::string category);
 };
+
+PyGlobalSettings::PyGlobalSettings() {
+    py::module sys = py::module::import("sys");
+    py::list argv = sys.attr("argv");
+    m_settings.setId(argv[0].cast<std::string>());
+}
 
 SettingValue PyGlobalSettings::get(std::string key, std::string category, SettingValue def) {
     return m_settings.get(key, category, def);
 }
+
+SettingValue PyGlobalSettings::set(std::string key, SettingValue value, std::string category) {
+    return m_settings.set(key, value, category);
+}
+
 
 std::string PyGlobalSettings::__repr__() {
     std::string result = "GlobalSettings";
@@ -33,6 +46,12 @@ void PyGlobalSettings::add_python_binding(pybind11::module &m)
         py::arg("key"),
         py::arg("category") = "",
         py::arg("default") = SettingValue())
+        .def("set", [] (PyGlobalSettings &m, std::string key, SettingValue value, std::string category = "") {
+                return m.set(key, value, category);
+            },
+            py::arg("key"),
+            py::arg("value"),
+            py::arg("category") = "")
         .def("__repr__", &PyGlobalSettings::__repr__);
 }
 
